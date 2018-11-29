f986c2c5e1c564a7c27a0857a57df56e5ad736dc
caustin3 <43153579+caustin3@users.noreply.github.com>
2018-11-29 14:20:53 -0700

    Update hash.h

--- hash.h
+++ hash.h
@@ -29,37 +29,9 @@ using std::hex;
 //by taking the string and turning it in to a number set 
 //then uses a function to hash the number set 
 //last it sjanges the hashs number in to a string 
-string hash(string parent_ID , string raw_E)
-{ 
-  unsigned long int hash_number;// i thought it may overflow in the for loop CA
-  string output,to_hash;
-  to_hash = parent_ID + raw_E;
-  
-  for (int index =0; index <to_hash.length(); index++)
-  {
-    hash_number = 13*hash_number + to_hash[index]
-  }  
-  hash_number %= (UINT_MAX-1); // 
-  stringstream thing <<hex <<hash_number;
-  output = thing.str();
-  return output ;
-}
+string hash(string parent_ID , string raw_E);
 
-string hash(string parent_ID, string raw_E,string ID,string L_hash, string R_hash)
-{
-  int hash_number;
-  string output,to_hash;
-  to_hash = parent_ID + raw_E + ID + L_hash +R_hash;
-  
-  for (int index =0; index <to_hash.length(); index++)
-  {
-    hash_number = 13*hash_number + to_hash[index]
-  } 
-
- // The maximum number of 8-hex-character strings possible 
-  hash_number %= (UINT_MAX-1);
-  stringstream streamHex << hex <<hash_number;
-  output = streamHex.str();
+string hash(string parent_ID, string raw_E,string ID,string L_hash, string R_hash);
   
   
   return output;
