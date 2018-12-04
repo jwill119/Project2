341d16b1cc72fc672ccf8d4c6e0f2b5f533f2497
caustin3 <43153579+caustin3@users.noreply.github.com>
2018-12-04 12:02:48 -0700

    Update trackingtree.cpp

--- trackingtree.cpp
+++ trackingtree.cpp
@@ -48,6 +48,7 @@ void trackingtree::add_node()
     new_node.setEVENT(raw_event); 
     
     tree.push_back(new_node);
+    index = 1;
     number_of_nodes =1;
   }else 
   {
@@ -64,17 +65,32 @@ void trackingtree::add_node()
     new_node.setEVENT(raw_event); 
         
     tree.push_back(new_node);  
-    
-    number_of_nodes += 1;
+    index++;
+    number_of_nodes++;
   }   
-  update_tree();  
+  update_node(index);  
 }
 
-void trackingtree::update_node(int index);
+void trackingtree::update_node(int index_in);
 {
    //hashs the ID of the new node, and stores new ID 
-    string ID = hash(new_node.getPID(), new_node.getEVENT());
-    new_node.setID(ID);
+    string ID = hash(tree.at(index_in).getPID(),tree.at(index_in).getEVENT());
+    tree.at(index_in).updateID(ID);
+    if(number_of_node > 1)
+    {
+      if(index_in%2 == 0)
+      string update = hash(tree.at(index_in).getPID(), tree.at(index_in).getEVENT(),
+                           tree.at(index_in).getID(), tree.at(index_in)getLHASH(), 
+                           tree.at(index_in)getRHASH());      
+      tree.at((index_in/2)).updateLHASh(hash)
+    }else if(index_in%2 == 1)
+    {
+      string update = hash(tree.at(index_in).getPID(), tree.at(index_in).getEVENT(),
+                           tree.at(index_in).getID(), tree.at(index_in)getLHASH(), 
+                           tree.at(index_in)getRHASH());      
+      tree.at((index_in/2)).updateLHASh(hash)      
+    } 
+  update_node((index_in/2))
 }
 
 //void trackingtree::update_tree()
